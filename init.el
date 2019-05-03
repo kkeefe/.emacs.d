66a235e617f73820a666744427b26f02802c3b64
kkeefe <kevinpk@hawaii.edu>
2019-05-03 11:17:15 -1000

    inital commit from magit

--- init.el
+++ init.el
@@ -4,17 +4,44 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/configurations.org"))
 (provide 'init)
 ;;; init.el ends here
-
-;;; custom shit..
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-enabled-themes (quote (tango-dark)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (ox-reveal zenburn-theme which-key use-package try pylint powerline-evil org-evil msvc ipcalc htmlize help-find-org-mode function-args format-all flymake-ruby flycheck flex-isearch exec-path-from-shell evil-org evil-magit cpputils-cmake counsel company-irony-c-headers color-theme))))
+    (org-projectile zenburn-theme which-key use-package try pylint powerline-evil org-evil msvc ipcalc htmlize help-find-org-mode function-args format-all flymake-ruby flycheck flex-isearch exec-path-from-shell evil-org evil-magit cpputils-cmake counsel company-irony-c-headers color-theme)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
